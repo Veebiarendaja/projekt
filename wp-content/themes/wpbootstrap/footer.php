624c8447da5bba61b6de5d03d3fd4caaf5b85811
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-10-01 23:19:55 +0300

    Wordpress

---
+++ wp-content/themes/wpbootstrap/footer.php
@@ -0,0 +1,9 @@
+<hr>
+<div class="footer-wrap">
+    <p>FOOTER AREA</p>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/wpbootstrap/header.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
+</head>
+<body>
+<div class="header-wrap">
+    <div class="navigation-wrap">
+        <div class="menu-block1">
+            <div class="menu_side0">
+                <a href="" style="padding-right: 40px">
+                    <li>Gallery</li>
+                </a>
+                <a href="">
+                    <li>Elandilim</li>
+                </a>
+            </div>
+        </div><div class="menu-block-logo">
+            <div class="pea_logo">
+                <img src="../wordpress/wp-content/themes/wpbootstrap/img/logo.jpg" alt="">
+            </div>
+        </div>
+        <div class="menu-block2">
+            <div class="menu_side1">
+                <a href="" style="padding-right: 40px">
+                    <li>Life</li>
+                </a>
+                <a href="">
+                    <li>Home</li>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
